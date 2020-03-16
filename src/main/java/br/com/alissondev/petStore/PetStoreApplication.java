6298a56c10a1428d134751088c2edcf0c9a6ccca
Alisson Youssf <youssfbr@gmail.com>
2020-03-15 22:06:54 -0300

    Projeto criado

---
+++ src/main/java/br/com/alissondev/petStore/PetStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.alissondev.petStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alissondev/petStore/PetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alissondev.petStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
